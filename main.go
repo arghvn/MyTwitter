c585c5ed648de9e044695705625fc8c946bcc3d2
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-06-07 15:22:05 +0430

    start

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+//My first project after passing the learning stage
+
+//Using the Twigo library and the scribble package,
+//we want to capture the identities of ID people on Twitter and save them on Scribble.
+
+//Twigo from github , written by user arshamalh
+//scribble from json database
+//scribble is a Simple JSON Database in Golang
+
+//To use Tiwgo, we need items such as consumer key, access token, bearer token, which we need to create.
+
+
+func main () {
+	godotenv,load()
+	
+}
