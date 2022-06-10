65211780a2b8c1b5a8a622daca506309396d6ac0
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-06-10 11:56:13 +0430

    finifsh this relsted Practice

--- Base64_Encoding.go
+++ Base64_Encoding.go
@@ -1,13 +1,22 @@
 package main
 
+//Go provides built-in support for base64 encoding/decoding.
+
 import (
 	b64 "encoding/base64"
 	"fmt"
 )
 
+// This syntax imports the encoding/base64 package with the b64 name instead of the default base64.
+//  It’ll save us some space below.Here’s the string we’ll encode/decode.
+
 func main() {
 
 	data := "abc123!?$*&()'-=@~"
+
+	// Go supports both standard and URL-compatible base64.
+	// Here’s how to encode using the standard encoder. The encoder requires a []byte
+	//  so we convert our string to that type.
 
 	sEnc := b64.StdEncoding.EncodeToString([]byte(data))
 	fmt.Println(sEnc)
@@ -16,8 +25,22 @@ func main() {
 	fmt.Println(string(sDec))
 	fmt.Println()
 
+	// Decoding may return an error, which you can check if you don’t
+	//  already know the input to be well-formed.
+
 	uEnc := b64.URLEncoding.EncodeToString([]byte(data))
 	fmt.Println(uEnc)
 	uDec, _ := b64.URLEncoding.DecodeString(uEnc)
 	fmt.Println(string(uDec))
 }
+
+// This encodes/decodes using a URL-compatible base64 format.
+// The string encodes to slightly different values with the standard and URL
+// base64 encoders (trailing + vs -) but they both decode to the original string as desired.
+
+// output :
+
+// YWJjMTIzIT8kKiYoKSctPUB+
+// abc123!?$*&()'-=@~
+// YWJjMTIzIT8kKiYoKSctPUB-
+// abc123!?$*&()'-=@~
